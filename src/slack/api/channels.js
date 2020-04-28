6ca18d8dafb3b9d5846707d4ac3671769943fbe4
Alexander Svinarev <shibanet0@gmail.com>
2020-04-28 21:01:59 +0300

    change method slack API channels -> conversations

--- src/slack/api/channels.js
+++ src/slack/api/channels.js
@@ -3,9 +3,22 @@ const env = require('../../env');
 const web = new WebClient(env.getSlackToken());
 
 function list() {
-  // TODO: This method is deprecated! Fix this
-
-  return web.channels.list();
+  return new Promise((resolve, reject) => {
+    web.conversations
+      .list()
+      .then((resp) => {
+        Promise.all(
+          resp.channels.map(async (chan) =>
+            web.conversations.members({ channel: chan.id }).then((resp) => resp.members),
+          ),
+        )
+          .then((arr) => {
+            resolve({ ...resp, channels: resp.channels.map((chan, id) => ({ ...chan, members: arr[id] })) });
+          })
+          .catch((e) => reject(e));
+      })
+      .catch((e) => reject(e));
+  });
 }
 
 module.exports = { list };
